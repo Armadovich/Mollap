6fac4e1f5a40beb564ea017095a0baca4cbdbda5
Armadovich+ <armada.dag@gmail.com>
2024-03-15 03:10:35 +0100

    ci(.gitignore,pom.xml,App,AppTest):add initial files

---
+++ src/test/java/edu/gorilas/AppTest.java
@@ -0,0 +1,13 @@
+package edu.gorilas;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class AppTest {
+
+
+
+
+}
